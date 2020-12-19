cef202feae3c0144fa8732d85613f240b39409c0
Hyper1025 <leo_xarroweb@hotmail.com>
2020-12-19 15:42:45 -0300

    :zap: Melhorias no log do console
    Mudanças:
    -Adicionado cores ao log
    -Adicionado animações (só funcionam no terminal)
    -Pequena alteração no formato do log
    -Codigos comentados foram movidos para o final do script

--- index.js
+++ index.js
@@ -3,13 +3,26 @@
 require('dotenv').config(); // Biblioteca "dotenv" carrega variáveis ​​de ambiente de um arquivo .env
 const Twitter = require('twitter-lite'); // Biblioteca cliente/servidor para a API do Twitter.
 const checkInternetConnected = require('check-internet-connected'); // Biblioteca que verifica se estamos conectados à Internet ou não
-const ora = require('ora'); // Cria animações no console (embora por algum motivo não consegui fazer funcionar)
+const ora = require('ora'); // Cria animações no console (Só funciona no terminal)
 const figlet = require('figlet'); // Cria textos com fontes bem legais tipo os vistos aqui: http://patorjk.com/software/taag/
+const colors = require('colors');
+
+// Esquema de cores para o console
+colors.setTheme({
+  info: 'Blue',
+  help: 'cyan',
+  warn: 'yellow',
+  success: 'Green',
+  error: 'red'
+});
 
 // variáveis
 var dataAtual = new Date(); // Data atual
 var internetStatus = true; // Status da internet, por padrão vem como funcionando (verdadeiro)
 var dataDeQueda; // Data de queda
+var oraConsoleLogOnline = false; //
+var oraConsoleLogOffline = false; //
+var aguardandoRede = ora('Aguardando rede');
 
 // Nome do script que será impresso na tela ao inicializar
 console.log(figlet.textSync('CiacOff!', { font: "3D-ASCII" }));
@@ -24,13 +37,6 @@ const client = new Twitter({
   access_token_key: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.TOKEN_SECRET,
 });
-
-//client
-//  .get("account/verify_credentials")
-//  .then(results => {
-//    console.log("results", results);
-//  })
-//  .catch(console.error);
 
 // Construtor da configuração de testes de rede
 console.log(`[❕] ${DateTimeFormat(dataAtual)} - Inicializando construtor testes internet`);
@@ -57,24 +63,10 @@ function DateTimeFormat(date) {
 
   var hour = date.getHours();
   hour = (hour < 10 ? "0" : "") + hour;
-
   var min = date.getMinutes();
   min = (min < 10 ? "0" : "") + min;
-
   var sec = date.getSeconds();
   sec = (sec < 10 ? "0" : "") + sec;
-
-  /*
-  var year = date.getFullYear();
-
-  var month = date.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
-
-  var day  = date.getDate();
-  day = (day < 10 ? "0" : "") + day;
-
-  return day + "/" + month + "/" + year + " - " + hour + ":" + min + ":" + sec + " "; 
-  */
 
   return hour + ":" + min + ":" + sec;
 
@@ -86,11 +78,37 @@ console.log(figlet.textSync('----------', { horizontalLayout: 'default' }));
 
 // Loop para para realizar as funções
 setInterval(() => {
+  // cria um log no console animado, com um spinner que diz "Testando conexão"
+  var statusTest = ora('Testando conexão').start();
+
   // Testamos o estado da conexão
   checkInternetConnected(config)
     .then((result) => {
       // Atualiza o horário
       dataAtual = new Date;
+
+      // Verifica se o aguardandoRede está girando.
+      // Se estiver, quer dizer que a internet havia caído, e estava esperando por reconexão
+      if (aguardandoRede.isSpinning) {
+        // Ao reconectar, para a animação
+        aguardandoRede.stop();
+      } // Do contrário, só ignora para evitar erros
+
+      // Previne que seja gerado vários "Onlines" no console
+      // Verificamos se o log do online já está no console, através da variável `oraConsoleLogOnline`
+      // Caso ainda não tenha sido feito (false), enviamos o evento para o console
+      if (oraConsoleLogOnline == false) {
+        // a linha a baixo é responsável pos fazer o log do evento online acontecer
+        statusTest.succeed(`${DateTimeFormat(dataAtual)} - Online`);
+      } else {
+        // A instância stop, para o "statusTest" para e limpa a linha do console
+        // Assim só temos uma pequena animação gerada ao carregar o "statusTest", com a sua mensagem pré definida de "Testando conexão"
+        statusTest.stop();
+      }
+      // Aqui dizemos que o log de online já foi escrito...
+      // Agora, para que o "statusTest" de ONLINE possa ser escrito novamente, a internet terá de cair, para ele receber o status false, e então entrar no if aqui de cima
+      // Nota: A animação de teste ainda ocorre enquanto o checkInternetConnected estiver rodando
+      oraConsoleLogOnline = true;
 
       // verificamos o status prévio da internet salvo na variável "internetStatus" com um if statement
       // se for falso, quer dizer que estava-mos sem internet até a execução desse código
@@ -99,24 +117,34 @@ setInterval(() => {
 
         // para isso, usamos um try, junto com a função tweet, declarada lá em cima
         try {
+          // Como a conexão retornou, redefinimos as duas variáveis das animações
+          // oraConsoleLogOnline, do `statusTest` com a mensagem de ✔ online
+          // oraConsoleLogOffline, do `statusTest` com a mensagem de ✖ offline
+          oraConsoleLogOnline = false;
+          oraConsoleLogOffline = false;
           // log do console
-          console.log(`[🌎] ${DateTimeFormat(dataAtual)} - ` + result); // conexão de internet está online
-
+          console.log(`🌎 ${DateTimeFormat(dataAtual)} - Retornou ${result}`.success); // conexão de internet está online
           //faz o tweet
           tweet(`Atualização:\n😔 CiacOFF: ${DateTimeFormat(dataDeQueda)}\n🥰 CiacON: ${DateTimeFormat(dataAtual)}\n\nOs dados aqui apresentados não representam o status da rede como uma totalidade. Caso esteja enfrentando instabilidades, entre em contato com a provedora`)
             .then(internetStatus = true) // Ao acabar de fazer o tweet, alteramos o status da variável "internetStatus" para verdadeiro,           
-
           // e então joga pro log o evento
-          console.log(`[🐦] ${DateTimeFormat(dataAtual)} - Post`);
+          console.log(`🐦 ${DateTimeFormat(dataAtual)} - Post`.help);
         } catch (error) {
           // Caso haja erro, o escrevemos no console
-          console.log(error)
+          console.log(colors.red(error.message))
         }
       } // Fim do if  
     })
     .catch((ex) => {
       // Atualiza o horário
       dataAtual = new Date;
+
+      if (oraConsoleLogOffline == false) {
+        statusTest.fail(`${DateTimeFormat(dataAtual)} - Offline`);
+      } else {
+        statusTest.stop();
+      }
+      oraConsoleLogOffline = true;
 
       // se a execução cair aqui, quer dizer que obtivemos um erro ao tentar resolver o DNS do domínio declarado lá em cima.
       // Ou seja, estamos sem internet, pois não atingimos o servidor DNS
@@ -125,7 +153,9 @@ setInterval(() => {
       // precisamos verificar se a conexão estava ativa
       if (internetStatus == true) {
         // Sendo assim jogamos o status da rede para o log, avisando de que ela acabou de ficar offline
-        console.log(`[❌] ${DateTimeFormat(dataAtual)} - ` + ex);
+        console.log(`‼ ${DateTimeFormat(dataAtual)} - ${ex}`.error);
+        // Inicia a animação de aguardando rede
+        aguardandoRede.start();
         // Atribuímos ao "downDate" a data e hora atual, passando o "dataAtual",
         // que foi previamente atualizado com o horário da última consulta
         dataDeQueda = dataAtual;
@@ -144,3 +174,20 @@ setInterval(() => {
 //} catch (error) {
 //  console.log(error);
 //}
+
+//client
+//  .get("account/verify_credentials")
+//  .then(results => {
+//    console.log("results", results);
+//  })
+//  .catch(console.error);
+
+// Adicionar novamente na função `DateTimeFormat` caso queira retorno de ano, dia e mês
+/*
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+month = (month < 10 ? "0" : "") + month;
+var day  = date.getDate();
+day = (day < 10 ? "0" : "") + day;
+return day + "/" + month + "/" + year + " - " + hour + ":" + min + ":" + sec + " ";
+*/
